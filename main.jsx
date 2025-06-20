6cc73efeb271249e6edb20d072c81506baa7c577
bouwendijk <B.ouwendijk87@gmail.com>
2025-06-20 10:49:33 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,7 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => <h1>MBD Klimaatbeheer App Startpagina</h1>
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
