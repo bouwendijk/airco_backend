24dacd7ad8a2a7377952630cbcd9ec549c32129d
bouwendijk <B.ouwendijk87@gmail.com>
2025-06-20 10:18:23 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>MBD Klimaatbeheer</h1>
+      <p>Welkom bij het dashboard voor klantenbeheer en installaties.</p>
+    </div>
+  )
+}
+
+export default App
